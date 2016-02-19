4ec33676bf81dfc40815873e715754d8e8226606
Oleg Dudar <oleg_dudar@epam.com>
2016-02-19 17:18:21 +0200

    Initial commit

---
+++ app/src/main/java/com/olegdudar/bb8/MainActivity.java
@@ -0,0 +1,229 @@
+package com.olegdudar.bb8;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity  implements BluetoothAdapter.LeScanCallback{
+    private static final String TAG = "BluetoothGattActivity";
+
+    private static final String DEVICE_NAME = "BB-";
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private SparseArray<BluetoothDevice> mDevices;
+
+    private BluetoothGatt mConnectedGatt;
+
+    private ProgressDialog mProgress;
+
+    private TextView mac_Address;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_main);
+        setProgressBarIndeterminate(true);
+
+        mac_Address = (TextView) findViewById(R.id.device_mac);
+        mac_Address.setText("Please search for device");
+
+        /*
+         * Bluetooth in Android 4.3 is accessed via the BluetoothManager, rather than
+         * the old static BluetoothAdapter.getInstance()
+         */
+        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        mBluetoothAdapter = manager.getAdapter();
+
+        mDevices = new SparseArray<BluetoothDevice>();
+
+        /*
+         * A progress dialog will be needed while the connection process is
+         * taking place
+         */
+        mProgress = new ProgressDialog(this);
+        mProgress.setIndeterminate(true);
+        mProgress.setCancelable(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+         * We need to enforce that Bluetooth is first enabled, and take the
+         * user to settings to enable it if they have not done so.
+         */
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            //Bluetooth is disabled
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBtIntent);
+            finish();
+            return;
+        }
+
+        /*
+         * Check for Bluetooth LE Support.  In production, our manifest entry will keep this
+         * from installing on these devices, but this will allow test devices or other
+         * sideloads to report whether or not the feature exists.
+         */
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "No LE Support.", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Make sure dialog is hidden
+        mProgress.dismiss();
+        //Cancel any scans in progress
+        mHandler.removeCallbacks(mStopRunnable);
+        mHandler.removeCallbacks(mStartRunnable);
+        mBluetoothAdapter.stopLeScan(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Disconnect from any active tag connection
+        if (mConnectedGatt != null) {
+            mConnectedGatt.disconnect();
+            mConnectedGatt = null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Add the "scan" option to the menu
+        getMenuInflater().inflate(R.menu.main, menu);
+        //Add any device elements we've discovered to the overflow menu
+        for (int i=0; i < mDevices.size(); i++) {
+            BluetoothDevice device = mDevices.valueAt(i);
+            menu.add(0, mDevices.keyAt(i), 0, device.getName());
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_scan:
+                mDevices.clear();
+                startScan();
+                return true;
+            default:
+                //Obtain the discovered device to connect with
+                BluetoothDevice device = mDevices.get(item.getItemId());
+                Log.i(TAG, "Connecting to "+device.getName());
+                mac_Address.setText("BB-8 MAC: "+device.getAddress());
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private Runnable mStopRunnable = new Runnable() {
+        @Override
+        public void run() {
+            stopScan();
+        }
+    };
+    private Runnable mStartRunnable = new Runnable() {
+        @Override
+        public void run() {
+            startScan();
+        }
+    };
+
+    private void startScan() {
+        mBluetoothAdapter.startLeScan(this);
+        setProgressBarIndeterminateVisibility(true);
+
+        mHandler.postDelayed(mStopRunnable, 3000);
+    }
+
+    private void stopScan() {
+        mBluetoothAdapter.stopLeScan(this);
+        setProgressBarIndeterminateVisibility(false);
+    }
+
+    /* BluetoothAdapter.LeScanCallback */
+
+    @Override
+    public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+        Log.i(TAG, "New LE Device: " + device.getName() + " @ " + rssi);
+        /*
+         * We are looking for SensorTag devices only, so validate the name
+         * that each device reports before adding it to our collection
+         */
+        if (device.getName() != null && device.getName().startsWith(DEVICE_NAME)) {
+            mDevices.put(device.hashCode(), device);
+            //Update the overflow menu
+            invalidateOptionsMenu();
+            mac_Address.setText("BB-8 discovered!");
+        }
+    }
+
+    /*
+     * In this callback, we've created a bit of a state machine to enforce that only
+     * one characteristic be read or written at a time until all of our sensors
+     * are enabled and we are registered to get notifications.
+     */
+    private BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+
+        private String connectionState(int status) {
+            switch (status) {
+                case BluetoothProfile.STATE_CONNECTED:
+                    return "Connected";
+                case BluetoothProfile.STATE_DISCONNECTED:
+                    return "Disconnected";
+                case BluetoothProfile.STATE_CONNECTING:
+                    return "Connecting";
+                case BluetoothProfile.STATE_DISCONNECTING:
+                    return "Disconnecting";
+                default:
+                    return String.valueOf(status);
+            }
+        }
+    };
+
+    /*
+     * We have a Handler to process event results on the main thread
+     */
+    private static final int MSG_PROGRESS = 201;
+    private static final int MSG_DISMISS = 202;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_PROGRESS:
+                    mProgress.setMessage((String) msg.obj);
+                    if (!mProgress.isShowing()) {
+                        mProgress.show();
+                    }
+                    break;
+                case MSG_DISMISS:
+                    mProgress.hide();
+                    break;
+            }
+        }
+    };
+}
